4a745b55435f610f6fea8fa15d24f61e2297064a
gustavo5855 <gustavo.soares.cdc@gmail.com>
2017-10-28 18:56:26 -0300

    sumo

--- novo_teste/gerar_carros.sh
+++ novo_teste/gerar_carros.sh
@@ -2,6 +2,6 @@
 # argv[1] = numero de carros
 # argv[2] = numero de onibus
 # argv[3] = primeira linha de veiculos no arquivo teste.rou.xml para q ele apague os existentes e insira outros a partir dali
-python add_vehicle.py 30 30 21
+python add_vehicle.py 100 100 21
 # abre o sumo para rodar a simulacao
 sumo-gui teste.sumo.cfg
